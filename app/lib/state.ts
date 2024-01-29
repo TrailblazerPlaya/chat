28112194a4c1d5a21b882f552b8e02d241b03a1e
fornara <fornarapro@gmail.com>
2024-01-29 10:52:19 +0300

    Adaptive

--- app/lib/state.ts
+++ app/lib/state.ts
@@ -9,11 +9,6 @@ interface Message {
 	isBot?: boolean;
 }
 
-// interface ImageMessage extends Message {
-// 	type: "image";
-// 	imageUrl: string;
-// }
-
 interface ChatState {
 	messages: Message[];
 	botStatus: string;
