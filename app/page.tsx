a372976c63790315a8f03cea41f302cce8c71458
fornara <fornarapro@gmail.com>
2024-01-29 11:28:43 +0300

    Home page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { jost } from "@/app/ui/fonts";
+import Link from "next/link";
+import cls from "@/app/ui/page.module.scss";
 import "@/app/ui/global.scss";
 export default function Page() {
 	return (
-		<main className={` ${jost.className}`}>
-			<h1>Home Page</h1>
+		<main className={` ${jost.className} ${cls.Home}`}>
+			<div className={cls.CenterContent}>
+				Чат, который здоровается с миром.
+				<Link href={"/chat"} className={cls.Link}>
+					Перейти
+				</Link>
+			</div>
 		</main>
 	);
 }
